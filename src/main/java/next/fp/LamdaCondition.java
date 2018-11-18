1bf390ece2334f0b52f73ba032d80323fef86249
Jaenyeong-kim <jaenyeong.dev@gmail.com>
2018-11-18 21:11:27 +0900

    2018.11.18 lamda, filter, map, reduce, optional practice first commit

---
+++ src/main/java/next/fp/LamdaCondition.java
@@ -0,0 +1,5 @@
+package next.fp;
+
+public interface LamdaCondition {
+    boolean conditon(Integer number);
+}
--- src/main/java/next/fp/StreamStudy.java
+++ src/main/java/next/fp/StreamStudy.java
@@ -10,35 +10,51 @@ import java.util.stream.Collectors;
 
 public class StreamStudy {
 
-	public static long countWords() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+    public static long countWords() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-		long count = 0;
-		for (String w : words) {
-		  if (w.length() > 12) count++;  
-		}
-		return count;
-	}
-	
-	public static void printLongestWordTop100() throws IOException {
-		String contents = new String(Files.readAllBytes(Paths
-				.get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-		
-		// TODO 이 부분에 구현한다.
-	}
+        long count = 0;
+        for (String w : words) {
+          if (w.length() > 12) count++;
+        }
+        return count;
+    }
 
-	public static List<Integer> doubleNumbers(List<Integer> numbers) {
-		return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
-	}
+    public static void printLongestWordTop100() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths
+                .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-	public static long sumAll(List<Integer> numbers) {
-		return numbers.stream().reduce(0, (x, y) -> x + y);
-	}
+//	    System.out.println(contents);
 
-	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
-	}
+        // TODO 이 부분에 구현한다.
+//	    단어의 길이가 12자를 초과하는 단어를 추출한다.
+//	    12자가 넘는 단어 중 길이가 긴 순서로 100개의 단어를 추출한다.
+//		단어 중복을 허용하지 않는다. 즉, 서로 다른 단어 100개를 추출해야 한다.
+//	    추출한 100개의 단어를 출력한다. 모든 단어는 소문자로 출력해야 한다
+
+	    words.stream()
+			    .filter(x -> x.length() > 12)
+			    .sorted((a, b) -> a.length() - b.length())
+			    .distinct()
+			    .limit(100)
+			    .map(String::toLowerCase)
+			    .forEach(System.out::println);
+    }
+
+    public static List<Integer> doubleNumbers(List<Integer> numbers) {
+        return numbers.stream().map(x -> 2 * x).collect(Collectors.toList());
+    }
+
+    public static long sumAll(List<Integer> numbers) {
+        return numbers.stream().reduce(0, (x, y) -> x + y);
+    }
+
+    public static long sumOverThreeAndDouble(List<Integer> numbers) {
+    	return numbers.stream().filter(x -> x > 3)
+			    .map(x -> x * 2).collect(Collectors.toList())
+			    .stream().reduce(0, (x, y) -> x + y);
+    }
 }
